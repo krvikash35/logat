5cc3ca99e5b8db0766a5a8a25b55a983e4621eec
vikash <krvikash35@gmail.com>
2016-09-25 13:44:34 +0530

    added type test file

---
+++ type/logat-tests.ts
@@ -0,0 +1,16 @@
+/// <reference path="logat.d.ts" />
+
+import logat = require('logat')
+
+logat.error('This is error');
+logat.error( new Error('This is error instance') );
+logat.error( String('object1'), String('object2') );
+logat.warn('This is warn');
+logat.info('This is info');
+logat.debug('This is debug');
+
+logat.setOptions( {
+  logLevel: 4,
+  logMethod: 1
+});
+logat.getOptions();
